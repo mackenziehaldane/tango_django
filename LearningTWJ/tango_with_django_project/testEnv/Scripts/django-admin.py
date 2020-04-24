97aaa563cdbdec6b7974af80f006ae4858ca31db
mackenziehaldane <57261681+mackenziehaldane@users.noreply.github.com>
2020-04-24 11:25:05 +0100

    Chapter 3 with exercises

---
+++ LearningTWJ/tango_with_django_project/testEnv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\GA1\PycharmProjects\LearningTWJ\tango_with_django_project\testEnv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
