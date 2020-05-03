8818ca816b7a53903250860ad2459497f5a6b314
mackenziehaldane <57261681+mackenziehaldane@users.noreply.github.com>
2020-05-03 15:44:49 +0100

    Chapter 8 with exercises completed.

--- LearningTWJ/tango_with_django_project/rango/views.py
+++ LearningTWJ/tango_with_django_project/rango/views.py
@@ -45,21 +45,20 @@ def show_category(request, category_name_slug):
 
 def add_category(request):
     form = CategoryForm()
-    # A HTTP POST?
+
     if request.method == 'POST':
         form = CategoryForm(request.POST)
-        # Have we been provided with a valid form?
+
         if form.is_valid():
-            # Save the new category to the database.
             cat = form.save(commit=True)
             print((cat, cat.slug))
-            # Now that the category is saved, we could confirm this.
-            # For now, just redirect the user back to the index view.
-            return redirect('/rango/')
-    else:
-        # The supplied form contained errors -
-        # just print them to the terminal.
-        print(form.errors)
+
+            return redirect(reverse('rango:index'))
+
+        else:
+            # The supplied form contained errors -
+            # just print them to the terminal.
+            print(form.errors)
     # Will handle the bad form, new form, or no form supplied cases.
     # Render the form with error messages (if any).
     return render(request, 'rango/add_category.html', {'form': form})
@@ -72,7 +71,7 @@ def add_page(request, category_name_slug):
         category = None
 
     if category is None:
-        return redirect('/rango/')
+        return redirect(reverse('rango:index'))
 
     form = PageForm()
 
